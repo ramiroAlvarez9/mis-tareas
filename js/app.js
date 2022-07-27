a14bc20ee242af3b63551a7b62de2123c51d8e7e
ramiroAlvarez9 <ramiro.al.alvarez@gmail.com>
2022-07-27 20:02:15 -0300

    nueva actualizacion de codigo

--- js/app.js
+++ js/app.js
@@ -1,83 +1,87 @@
-var fecha_hora = function () {
-    var fecha = document.querySelector('.main__header--container--items--h4');
-    var hoy = moment().format('LL');
-    var fecha_y_hora = fecha.textContent = hoy;
-    return fecha_y_hora;
-};
-var desplegarMenuForm = function () {
-    var tareasItems = document.querySelector('.tareas__container');
-    var menu = document.querySelector('.iconify-container-menu');
-    var menuForm = document.querySelector('#agregar-tarea');
-    // este evento hace aparecer el formulario, y elimina las tareas con display:block
-    menu.addEventListener('click', function () {
-        menuForm.style.transform = 'translateX(0)';
-        tareasItems.style.display = 'none';
-    });
-};
-var ocultarMenuForm = function () {
-    var tareasItems = document.querySelector('.tareas__container');
-    var arrow = document.querySelector('.iconify-container-arrow');
-    var menuForm = document.querySelector('#agregar-tarea');
-    // este evento hace desaparecer el formulario, y agrega otra vez las tareas con display:block
-    arrow.addEventListener('click', function () {
-        menuForm.style.transform = 'translateX(-100%)';
-        tareasItems.style.display = 'block';
-    });
-};
-var agregarTarea = function () {
-    var form = document.querySelector('.form');
-    //enviando formulario y guardando en local storage
-    form.addEventListener('submit', function (e) {
-        var input = document.querySelector('.form__input');
-        var select = document.querySelector('.select');
-        var tarea = input.value;
-        var prioridad = select.value;
-        var taskObject = {
-            tarea: tarea,
-            prioridad: prioridad
-        };
+/* Genera la fecha y hora con moment js, y la inserta dentro de "main__header--container--items--h4". */      
+const fechaYHora = () => document.querySelector('.main__header--container--items--h4').textContent = moment().format('LL');
+const funcionalidadesDelMenu = ()=> {
+  
+    const openButtonMenu = document.querySelector('.iconify-container-menu')
+    const closeButtonMenu = document.querySelector('.iconify-container-arrow')
+    
+    // este evento hace aparecer el formulario
+    openButtonMenu.addEventListener('click', () => {    
+        document.querySelector('#agregar-tarea').style.transform = 'translateX(0)';
+    })
+
+    // este evento hace desaparecer el formulario
+    closeButtonMenu.addEventListener('click', () => {
+        document.querySelector('#agregar-tarea').style.transform = 'translateX(-100%)'    
+    })
+}
+const agregarTarea = () => {
+ 
+    //toma los valores del formulario ".form" y los guarda en localStorage.
+    document.querySelector('.form').addEventListener('submit', () => {
+
+        const tarea  = document.querySelector('.form__input').value;
+        const prioridad  = document.querySelector('.select').value;
+
+        let taskObject = {
+            tarea,
+            prioridad,
+        }
+
         if (localStorage.getItem('tasks') === null) {
-            var tareas = [];
-            tareas.push(taskObject);
-            localStorage.setItem('tasks', JSON.stringify(tareas));
-        }
-        else {
-            var tasks = JSON.parse(localStorage.getItem('tasks'));
+            let tareas = []
+            tareas.push(taskObject)
+            localStorage.setItem('tasks', JSON.stringify(tareas))
+        } else {
+            let tasks = JSON.parse(localStorage.getItem('tasks'));
             tasks.push(taskObject);
-            localStorage.setItem('tasks', JSON.stringify(tasks));
+            localStorage.setItem('tasks', JSON.stringify(tasks))
         }
+
         mostrarTareas();
-    });
-};
-var mostrarTareas = function () {
-    var tareasLocalStorage = JSON.parse(localStorage.getItem('tasks'));
-    var tareasContainer = document.querySelector('.tareas__container');
-    for (var i = 0; i < (tareasLocalStorage === null || tareasLocalStorage === void 0 ? void 0 : tareasLocalStorage.length); i++) {
-        var tarea = tareasLocalStorage[i].tarea;
-        var prioridad = tareasLocalStorage[i].prioridad;
-        tareasContainer.innerHTML += "\n    <div class =\"tareas__container--items\">\n            <h6 class =\"tareas__container--title\">" + tarea + "</h6>\n            <div class =\"tareas__container--prioridad\" >prioridad: " + prioridad + "</div>\n            <div class = \"tareas__container--delete\">\n            <a href = \"/\" class = \"tareas__container--delete--button\" onclick = \"eliminarTarea('" + tarea + "')\"><div class=\"iconify\" data-icon=\"el:ok-circle\" data-inline=\"false\" style=\"color:#4957A2; width = \"50\" height = \"50\"></div></a>\n            </div>\n   \n    </div>\n   \n    ";
+
+    })
+}
+
+const cantidadDeTareas = () => 
+    document.querySelector('.main__header--container--number--h4').textContent = JSON.parse(localStorage.getItem('tasks')).length;
+
+const mostrarTareas = () => {
+
+    let tareasLocalStorage  = JSON.parse(localStorage.getItem('tasks'));
+
+    const tareasContainer = document.querySelector('.tareas__container')
+
+    for (let i  = 0; i < tareasLocalStorage.length; i++) {
+        let tarea  = tareasLocalStorage[i].tarea;
+        let prioridad = tareasLocalStorage[i].prioridad;
+
+        tareasContainer.innerHTML += `
+            <div class ="tareas__container--items">
+                    <h6 class ="tareas__container--title">${tarea}</h6>
+                    <div class ="tareas__container--prioridad" >prioridad: ${prioridad}</div>
+                    <div class = "tareas__container--delete">
+                    <a href = "/" class = "tareas__container--delete--button" onclick = "eliminarTarea('${tarea}')"><div class="iconify" data-icon="el:ok-circle" data-inline="false" style="color:#4957A2; width = "50" height = "50"></div></a>
+                </div>
+            </div>
+    `
     }
-};
-var cantidadDeTareas = function () {
-    if (JSON.parse(localStorage.getItem('tasks')) != null) {
-        var numeroDeTareas = document.querySelector('.main__header--container--number--h4');
-        var cantidadTareasLocalStorage = JSON.parse(localStorage.getItem('tasks')).length;
-        numeroDeTareas.textContent = cantidadTareasLocalStorage;
-    }
-};
-var eliminarTarea = function (tarea) {
-    var tareasLocalStorage = JSON.parse(localStorage.getItem('tasks'));
-    for (var i = 0; i < tareasLocalStorage.length; i++) {
+}
+const eliminarTarea = tarea  => {
+    let tareasLocalStorage = JSON.parse(localStorage.getItem('tasks'));
+
+    for (let i = 0; i < tareasLocalStorage.length; i++) {
+
         if (tareasLocalStorage[i].tarea === tarea) {
             tareasLocalStorage.splice(i, 1);
         }
     }
     localStorage.setItem('tasks', JSON.stringify(tareasLocalStorage));
-};
-window.localStorage;
+}
+
+fechaYHora();
+funcionalidadesDelMenu();
+agregarTarea();
 mostrarTareas();
 cantidadDeTareas();
-fecha_hora();
-desplegarMenuForm();
-ocultarMenuForm();
-agregarTarea();
+
